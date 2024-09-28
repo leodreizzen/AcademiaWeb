7d98f581f0d891772b4bcafe3c0f34d250f8037e
martoAs <martinaasteasuain22@gmail.com>
2024-09-27 21:20:34 -0300

    comienzo de front de alta de alumno y padre

--- src/app/(loggedin)/student/add/page.tsx
+++ src/app/(loggedin)/student/add/page.tsx
@@ -1,15 +1,10 @@
-/*
-* Imports necesarios
-* */
+import {StudentRegistrationFormComponent} from "@/app/(loggedin)/student/add/student-registration-form";
+
 
 export default function AddStudentPage() {
     return (
-        <div className=" w-full flex flex-col items-center justify-center min-h-screen relative">
-            <div className=" absolute">
-                <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-                </div>
-                se agregaAaAa el student
-            </div>
-        </div>
+
+                <StudentRegistrationFormComponent/>
+
     );
 }

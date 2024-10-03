0ce1c3be9116f1c985f192d4b8d9d8955d81dca7
Matias Nahuel Morata <nahuelmorata@gmail.com>
2024-10-03 15:06:52 -0300

    refactor: uso el icono del basurero para borrar

--- src/components/list/ListStudents.tsx
+++ src/components/list/ListStudents.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent } from "@/components/ui/card"
-import { Search, Edit, Eye, Plus } from 'lucide-react'
+import { Search, Edit, Eye, Plus, Trash2 } from 'lucide-react'
 import PaginationControls from "@/components/list/PaginationControls"
 import { StudentWithUser } from "@/app/(loggedin)/student/data"
 import { usePathname, useRouter } from "next/navigation"
@@ -110,7 +110,7 @@ export function ListStudents({ data, count }: PrincipalProps) {
                         <Eye className="mr-2 h-4 w-4" /> Ver
                       </Button>
                       <Button variant="outline" size="sm" onClick={() => handleRemove(student.id)} className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
-                        <Eye className="mr-2 h-4 w-4" /> Borrar
+                        <Trash2 className="mr-2 h-4 w-4" /> Borrar
                       </Button>
                     </div>
                   </CardContent>

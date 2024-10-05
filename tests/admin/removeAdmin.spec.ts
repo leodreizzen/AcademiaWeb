2993a30296c64aed0c52a480a641db96bd94ef31
Julian Gallardo <juliangallardo26603@gmail.com>
2024-10-05 18:48:52 -0300

    test remove admin

---
+++ tests/admin/removeAdmin.spec.ts
@@ -0,0 +1,93 @@
+import { test, expect } from '@playwright/test';
+import { login } from '@/helpersTest/loginHelper';
+import { searchAdminByDni } from '@/helpersTest/adminHelper';
+import { randomDNI } from '@/helpersTest/studentHelper';
+import { faker } from '@faker-js/faker';
+
+test.describe('Remove admin', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    })
+
+    test('Eliminar admin, luego no se puede logear ', async ({ page }) => {
+        await login(page, '33333333', 'admin');
+        await page.waitForURL('/');
+        await page.locator('a:has-text("Administradores")').click();
+        await page.waitForURL('/admin', { waitUntil: 'domcontentloaded' });
+        //await page.locator('a:has-text("Nuevo")').click(); deberia de cambiarse cuando se agregue el boton de nuevo
+        await page.locator('button:has-text("Agregar administrador")').click();
+        //await page.waitForURL('/admin/new',{waitUntil: 'domcontentloaded'});
+        const DNI = await randomDNI();
+        await page.fill('input[id="input-dni"]', DNI);
+        await page.fill('input[id="input-firstName"]', faker.person.firstName());
+        await page.fill('input[id="input-lastName"]', faker.person.lastName());
+        await page.fill('input[id="input-phoneNumber"]', faker.phone.number({ style: 'international' }));
+        await page.fill('input[id="input-email"]', faker.internet.email());
+        await page.fill('input[id="input-address"]', faker.location.direction());
+
+        page.on('dialog', dialog => {
+            expect(dialog.message()).toBe('El admin se ha registrado correctamente');
+            dialog.dismiss();
+        }
+        );
+
+        await page.locator('button[type="submit"]').click();
+
+        await page.waitForURL('/admin', { waitUntil: 'domcontentloaded' });
+
+
+
+
+        await expect(page.getByText('Gabriela Rodríguez HurtadoDNI: 33333333VerEditarBorrar')).toBeVisible();
+        expect(await searchAdminByDni(page, DNI)).toBe(true);
+
+
+        const menuButton = page.locator('button[aria-haspopup="menu"]');
+        await menuButton.click();
+
+        await page.waitForTimeout(300);
+
+        const logoutButton = page.locator('text=Cerrar sesión');
+        await logoutButton.click();
+
+        await page.waitForURL('/login');
+        await expect(page.locator('h3:has-text("Iniciar sesión")')).toBeVisible();
+
+        await login(page, DNI, DNI.toString());
+
+        await page.waitForURL('/');
+
+        console.log('Admin nuevo logeado correctamente');
+        await logoutButton.click();
+
+        await page.waitForURL('/login');
+        await expect(page.locator('h3:has-text("Iniciar sesión")')).toBeVisible();
+
+        await login(page, '33333333', 'admin');
+        await page.waitForURL('/');
+        await page.locator('a:has-text("Administradores")').click();
+
+        await searchAdminByDni(page, DNI);
+        await page.locator('button:has-text("Borrar")').click();
+
+        await page.waitForTimeout(300);
+
+
+        await menuButton.click();
+
+        await page.waitForTimeout(300);
+
+        await logoutButton.click();
+
+        await page.waitForURL('/login');
+
+        await page.fill('input[name="dni"]', DNI);
+        await page.fill('input[name="password"]', DNI.toString());
+        await page.click('button[type="submit"]');
+
+        expect (await page.locator('text=El usuario no existe').isVisible()  || await page.locator('text=La contraseña ingresada es incorrecta')).toBe(true);
+
+
+    });
+});

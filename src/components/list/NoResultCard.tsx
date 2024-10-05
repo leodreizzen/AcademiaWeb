ce11e77de3a01b7b384023c958b97a96267f901d
martoAs <martinaasteasuain22@gmail.com>
2024-10-05 15:31:04 -0300

    typo pero al final deje resultados

--- src/components/list/NoResultCard.tsx
+++ src/components/list/NoResultCard.tsx
@@ -5,7 +5,7 @@ export function NoResultCard() {
     return <Card key="show" className="bg-gray-700">
         <CardContent className="flex items-center justify-between p-3">
             <div>
-                <p className="font-semibold text-white text-xl">No se encontraron responables</p>
+                <p className="font-semibold text-white text-xl">No se encontraron resultados</p>
             </div>
         </CardContent>
     </Card>;

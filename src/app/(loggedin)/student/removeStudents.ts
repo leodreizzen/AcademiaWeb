1584140eeacca430977e9534f8b6c8e47aefadb8
Matias Nahuel Morata <nahuelmorata@gmail.com>
2024-10-06 17:41:27 -0300

    feat: agrego borrar usuario cuando se elimina un estudiante

--- src/app/(loggedin)/student/removeStudents.ts
+++ src/app/(loggedin)/student/removeStudents.ts
@@ -5,11 +5,31 @@ import { getCurrentProfilePrismaClient } from "@/lib/prisma_utils";
 export async function removeStudent(id: number) {
     const prisma = await getCurrentProfilePrismaClient();
     try {
+        const student = await prisma.student.findUnique({
+            where: {
+                id
+            }
+        });
         await prisma.student.delete({
             where: {
                 id
             }
         });
+        const user = await prisma.user.findUnique({
+            where: {
+                dni: student!.dni
+            },
+            include: {
+                profiles: true
+            }
+        });
+        if (user?.profiles.length === 0) {
+            await prisma.user.delete({
+                where: {
+                    dni: student!.dni
+                }
+            });
+        }
         return true;
     } catch (error) {
         console.error("Error fetching administrators:", error);

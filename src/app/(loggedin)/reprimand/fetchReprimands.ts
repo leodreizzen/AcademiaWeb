ce82fffad7f7890ed6f669fe39cd3807422bc496
leodreizzen <leodreizzen@gmail.com>
2024-10-14 14:51:24 -0300

    Order en listado de amonestaciones

--- src/app/(loggedin)/reprimand/fetchReprimands.ts
+++ src/app/(loggedin)/reprimand/fetchReprimands.ts
@@ -3,26 +3,24 @@ import {getCurrentProfilePrismaClient} from "@/lib/prisma_utils";
 import {fetchCurrentUser} from "@/lib/data/users";
 import {fetchSelectedChild} from "@/lib/data/children";
 
-export async function fetchReprimands({page, init, end}:{page: number, init?: Date, end?: Date}) {
+export async function fetchReprimands({page, init, end}: { page: number, init?: Date, end?: Date }) {
     const prisma = await getCurrentProfilePrismaClient()
     const NUMBER_OF_REPRIMANDS = 5;
     const profile = await fetchCurrentUser();
     try {
         if (profile != null) {
             let id;
-            if(profile.role == "Student")
+            if (profile.role == "Student")
                 id = profile.id;
-            else if(profile.role == "Parent") {
+            else if (profile.role == "Parent") {
                 const selectedChild = await fetchSelectedChild();
-                if(selectedChild != null)
+                if (selectedChild != null)
                     id = selectedChild.id;
                 else
                     return [];
-            }
-            else if(profile.role == "Teacher"){
+            } else if (profile.role == "Teacher") {
                 id = undefined;
-            }
-            else{
+            } else {
                 console.error("Error fetching reprimands: User is not a student or parent");
                 return [];
             }
@@ -43,24 +41,26 @@ export async function fetchReprimands({page, init, end}:{page: number, init?: Da
             } : {};
 
 
-
             return await prisma.reprimand.findMany({
-                    skip: (page - 1) * NUMBER_OF_REPRIMANDS,
-                    take: NUMBER_OF_REPRIMANDS,
-                    where: {
-                        ...idFilter,
-                        ...dateFilter,
+                skip: (page - 1) * NUMBER_OF_REPRIMANDS,
+                take: NUMBER_OF_REPRIMANDS,
+                where: {
+                    ...idFilter,
+                    ...dateFilter,
+                },
+                include: {
+                    teacher: {
+                        include: {
+                            user: true
+                        }
                     },
-                    include: {
-                        teacher: {
-                            include: {
-                                user: true
-                            }
-                        },
-                    }
-                });
-            }
-        else {
+                },
+                orderBy: {
+                    dateTime: "desc"
+                }
+
+            });
+        } else {
             return [];
         }
 
@@ -76,16 +76,15 @@ export async function countReprimands(init?: Date, end?: Date) {
     try {
         if (profile != null) {
             let id;
-            if(profile.role == "Student")
+            if (profile.role == "Student")
                 id = profile.id;
-            else if(profile.role == "Parent") {
+            else if (profile.role == "Parent") {
                 const selectedChild = await fetchSelectedChild();
-                if(selectedChild != null)
+                if (selectedChild != null)
                     id = selectedChild.id;
                 else
                     return 0;
-            }
-            else{
+            } else {
                 console.error("Error counting reprimands: User is not a student or parent");
                 return 0;
             }
@@ -106,15 +105,13 @@ export async function countReprimands(init?: Date, end?: Date) {
             } : {};
 
 
-
             return await prisma.reprimand.count({
                 where: {
                     ...idFilter,
                     ...dateFilter,
                 }
             });
-        }
-        else {
+        } else {
             return 0;
         }
 

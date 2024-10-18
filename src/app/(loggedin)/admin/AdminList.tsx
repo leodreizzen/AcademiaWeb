93cf9a7ada74e34bb915f86cbe6303ca85f4925e
leodreizzen <leodreizzen@gmail.com>
2024-10-18 18:25:52 -0300

    Cambios en botones

--- src/app/(loggedin)/admin/AdminList.tsx
+++ src/app/(loggedin)/admin/AdminList.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import { useEffect, useState } from "react";
-import { AdministatorUser, AdminQuery } from "./types";
-import { getAdmins, getTotalAdmins, removeAdmin } from "./adminActions";
+import {useEffect, useState} from "react";
+import {AdministatorUser, AdminQuery} from "./types";
+import {getAdmins, getTotalAdmins, removeAdmin} from "./adminActions";
 import AdminItem from "./adminItem";
-import { usePathname, useRouter } from "next/navigation";
-import { Plus, Search } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {usePathname, useRouter} from "next/navigation";
+import {Plus, Search, Trash2} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
 import {NoResultCard} from "@/components/list/NoResultCard";
 import {ADMINS_PER_PAGE} from "@/lib/data/pagination";
+import {Tooltip} from "@nextui-org/tooltip";
 
 interface AdminListProps {
     pageQuery?: number;
@@ -17,14 +18,18 @@ interface AdminListProps {
     lastNameQuery?: string;
 }
 
-export default function AdminList({ pageQuery, dniQuery, lastNameQuery }: AdminListProps) {
+export default function AdminList({pageQuery, dniQuery, lastNameQuery}: AdminListProps) {
     const [page, setPage] = useState(pageQuery ?? 1);
     const [dni, setDni] = useState<string | undefined>(dniQuery ?? undefined);
     const [lastName, setLastName] = useState<string | undefined>(dniQuery != null ? undefined : ((lastNameQuery && lastNameQuery.length > 0) ? lastNameQuery : undefined));
     const [administrators, setAdministrators] = useState<AdministatorUser[]>([]);
     const [totalPages, setTotalPages] = useState(0);
-    const [searchQuery, setSearchQuery] = useState<AdminQuery>({ page, dni: (dni == undefined || dni.length == 0) ? undefined : parseInt(dni), lastName });
-    const { replace, push } = useRouter();
+    const [searchQuery, setSearchQuery] = useState<AdminQuery>({
+        page,
+        dni: (dni == undefined || dni.length == 0) ? undefined : parseInt(dni),
+        lastName
+    });
+    const {replace, push} = useRouter();
     const pathname = usePathname();
     const [noResults, setNoResults] = useState(false);
 
@@ -38,14 +43,18 @@ export default function AdminList({ pageQuery, dniQuery, lastNameQuery }: AdminL
     useEffect(() => {
         const fetchAdministrators = async () => {
             const administratorsFromDB = await getAdmins({...searchQuery, page});
-            administratorsFromDB.length===0 ? setNoResults(true) : setNoResults(false);
+            administratorsFromDB.length === 0 ? setNoResults(true) : setNoResults(false);
             setAdministrators(administratorsFromDB);
         };
         fetchAdministrators();
     }, [searchQuery, page]);
 
     const searchAdministrator = () => {
-        setSearchQuery({ page, dni: (dni == undefined || dni.length == 0) ? undefined : parseInt(dni), lastName: (lastName && lastName.length > 0)? lastName : undefined });
+        setSearchQuery({
+            page,
+            dni: (dni == undefined || dni.length == 0) ? undefined : parseInt(dni),
+            lastName: (lastName && lastName.length > 0) ? lastName : undefined
+        });
         const params = new URLSearchParams({
             dni: dni ?? '',
             lastName: lastName ?? ''
@@ -61,7 +70,7 @@ export default function AdminList({ pageQuery, dniQuery, lastNameQuery }: AdminL
         setLastName(newLastname);
         setDni('');
     };
-    
+
     const handleView = (id: number) => {
         push(`/admin/${id}`);
     };
@@ -82,9 +91,13 @@ export default function AdminList({ pageQuery, dniQuery, lastNameQuery }: AdminL
             <div className="p-8 bg-[#212937] rounded-lg">
                 <div className="flex justify-between">
                     <h2 className="font-extrabold text-2xl">Busqueda de administradores</h2>
-                    <Button onClick={handleAdd} variant="secondary" className="bg-green-600 hover:bg-green-500 text-white">
-                        <Plus className="mr-2 h-4 w-4" /> Nuevo administrador
-                    </Button>
+
+                    <Tooltip content="Nuevo administrador" classNames={{content: "text-white"}}>
+                        <Button onClick={handleAdd} variant="secondary"
+                                className="bg-green-600 hover:bg-green-500 text-white">
+                            <Plus className="h-4 w-4"/>
+                        </Button>
+                    </Tooltip>
                 </div>
                 <div className="mt-8">
                     <Input
@@ -103,16 +116,19 @@ export default function AdminList({ pageQuery, dniQuery, lastNameQuery }: AdminL
                         onChange={e => handleChangeLastname(e.target.value)}
                         className="bg-gray-700 text-white placeholder-gray-400 border-gray-600 flex-grow text-lg py-2 sm:py-5"
                     />
-                    <Button onClick={searchAdministrator} variant="secondary" className="bg-gray-600 hover:bg-gray-500 px-5 w-full sm:w-auto">
-                        <Search className="h-5 w-5" />
+                    <Tooltip content="Buscar" classNames={{content: "text-white"}}>
+                    <Button onClick={searchAdministrator} variant="secondary"
+                            className="bg-gray-600 hover:bg-gray-500 px-5 w-full sm:w-auto">
+                        <Search className="h-5 w-5"/>
                     </Button>
+                    </Tooltip>
                 </div>
                 <div className="flex flex-col mt-8 gap-4">
                     {noResults && <NoResultCard user={"administradores"}/>}
                     {
                         administrators.map(administrator => (
                             <AdminItem key={administrator.id} administrator={administrator} onView={handleView}
-                                onEdit={handleEdit} onRemove={handleRemove} />
+                                       onEdit={handleEdit} onRemove={handleRemove}/>
                         ))
                     }
                 </div>
--- src/app/(loggedin)/admin/adminItem.tsx
+++ src/app/(loggedin)/admin/adminItem.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { AdministatorUser } from "./types";
-import { Edit, Eye, Trash2 } from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {AdministatorUser} from "./types";
+import {Edit, Eye, Trash2} from "lucide-react";
+import {Tooltip} from "@nextui-org/tooltip";
 
 interface AdminItemParams {
     administrator: AdministatorUser;
@@ -9,7 +10,7 @@ interface AdminItemParams {
     onRemove: (id: number) => void;
 }
 
-export default function AdminItem({ administrator, onView, onEdit, onRemove }: AdminItemParams) {
+export default function AdminItem({administrator, onView, onEdit, onRemove}: AdminItemParams) {
     return (
         <div className="border border-white rounded-lg p-4 flex bg-[#394150] gap-12 test-admin-item">
             <div className="flex flex-col grow">
@@ -19,15 +20,25 @@ export default function AdminItem({ administrator, onView, onEdit, onRemove }: A
                 <p className="text-sm mt-2">DNI: {administrator.user.dni}</p>
             </div>
             <div className="flex items-center gap-4">
-                <Button variant="outline" size="sm" onClick={() => onEdit(administrator.id)} className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
-                    <Edit className="mr-2 h-4 w-4" /> Editar
-                </Button>
-                <Button variant="outline" size="sm" onClick={() => onView(administrator.id)} className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
-                    <Eye className="mr-2 h-4 w-4" /> Ver
-                </Button>
-                <Button variant="outline" size="sm" onClick={() => onRemove(administrator.id)} className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
-                    <Trash2 className="mr-2 h-4 w-4" /> Borrar
-                </Button>
+                <Tooltip content="Editar" classNames={{content: "text-white"}}>
+                    <Button variant="outline" size="sm" onClick={() => onEdit(administrator.id)}
+                            className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
+                        <Edit className="h-4 w-4"/>
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Ver" classNames={{content: "text-white"}}>
+
+                    <Button variant="outline" size="sm" onClick={() => onView(administrator.id)}
+                            className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
+                        <Eye className="h-4 w-4"/>
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Borrar" classNames={{content: "text-white"}}>
+                    <Button variant="outline" size="sm" onClick={() => onRemove(administrator.id)}
+                            className="bg-gray-600 text-white hover:bg-gray-500 border-gray-500 flex-grow sm:flex-grow-0">
+                        <Trash2 className="h-4 w-4"/>
+                    </Button>
+                </Tooltip>
             </div>
         </div>
     );

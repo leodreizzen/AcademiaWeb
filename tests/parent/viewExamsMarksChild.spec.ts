162248dfd94e24d2ec4e47e67aad2e481cf797e6
leodreizzen <leodreizzen@gmail.com>
2024-11-01 19:29:41 -0300

    Arreglos en tests

--- tests/parent/viewExamsMarksChild.spec.ts
+++ tests/parent/viewExamsMarksChild.spec.ts
@@ -1,39 +1,41 @@
 import { expect, test } from "@playwright/test";
 import {loginAsTestUser} from "../testutils";
+import {getTestUser} from "../testdata";
 test.beforeEach(async ({page}) => {
     await page.goto('/');
 });
 
 
-const childWithExams = "Axel Berge" ;
-const childWithoutExams = "Toy Maggio";
+const child = getTestUser("secondStudent") ;
+const subjectWithExams = "Química";
+const subjectWithoutExams = "Computación";
 test.describe('Ver notas de hijos', () => {
 
     test('Seleccionar hijo con notas.', async ({ page }) => {
         await loginAsTestUser(page, 'secondParent');
-        await page.locator(`button:has-text("${childWithExams}")`).click();
+        await page.locator(`button:has-text("${child.firstName} ${child.lastName}")`).click();
         await page.waitForURL('/'); 
         await page.getByRole('navigation').getByRole('link', { name: 'Notas de exámenes' }).first().click();
      
-        const firstElement = await page.locator('div.space-y-4 > div').first();
-        const subjectNameBefore = await firstElement.locator('h2').innerText();
+        const firstElement = await page.locator('div.space-y-4 > div', {hasText: subjectWithExams});
         const verNotasButton = firstElement.locator('button');
         await verNotasButton.click();
       
         const modal = await page.locator('div[role="dialog"]');
         await expect(modal).toBeVisible();
         const subjectNameAfter = await modal.locator('h2').innerText();
-        expect(subjectNameAfter).toContain(subjectNameBefore);
+        expect(subjectNameAfter).toContain(subjectWithExams);
+        await expect (modal.locator('text=Exámen del día').first()).toBeVisible();
     });
 
     test('Seleccionar hijo sin notas.', async ({ page }) => {
         await loginAsTestUser(page, 'secondParent');
-        await page.locator(`button:has-text("${childWithoutExams}")`).click();
+        await page.locator(`button:has-text("${child.firstName} ${child.lastName}")`).click();
         await page.waitForURL('/'); 
         await page.getByRole('navigation').getByRole('link', { name: 'Notas de exámenes' }).first().click();
         await page.waitForTimeout(100);
 
-        const firstElement = await page.locator('div.space-y-4 > div').first();
+        const firstElement = await page.locator('div.space-y-4 > div', {hasText: subjectWithoutExams});
         const verNotasButton = firstElement.locator('button');
         await verNotasButton.click();
         await expect(page.locator('text=No hay exámenes registrados')).toBeVisible();

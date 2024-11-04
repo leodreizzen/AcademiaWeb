b0e70f28a68d51eb8d5c636574342d6ec4e0efec
Ian Sebalt <iansebalt@live.com.ar>
2024-11-04 15:28:26 -0300

    Fix para rol de profesor.

--- src/app/(loggedin)/attendance/[id]/page.tsx
+++ src/app/(loggedin)/attendance/[id]/page.tsx
@@ -4,11 +4,18 @@ import StudentAttendance from "@/components/ui/attendance/StudentAttendance";
 import {fetchSelectedChild} from "@/lib/data/children";
 import {getAttendanceForStudent} from "@/lib/actions/get-attendance";
 import {redirect} from "next/navigation";
+import {fetchCurrentUser} from "@/lib/data/users";
 
 
 export default async function ShowAttendance(){
 
     await assertPermission({resource: Resource.ATTENDANCE, operation: "READ"});
+
+    const user = await fetchCurrentUser();
+
+    if (!user) redirect('/403')
+
+    if (user.role != 'Parent') redirect('/403')
 
     const student = await fetchSelectedChild()
 

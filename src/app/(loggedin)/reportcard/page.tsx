63a652ed85d52e5bcd755a16628e83fb9ea708dd
leodreizzen <leodreizzen@gmail.com>
2024-11-10 20:11:28 -0300

    Arreglo de interfaz

--- src/app/(loggedin)/reportcard/page.tsx
+++ src/app/(loggedin)/reportcard/page.tsx
@@ -14,7 +14,7 @@ export default async function ReportCardInfoPage() {
     const user = await fetchCurrentUser();
     if (!user) {
         return (
-            <div className="min-h-screen bg-gray-900 text-gray-100 flex items-center justify-center p-4">
+            <div className="min-h-full bg-gray-900 text-gray-100 flex items-center justify-center p-4">
                 <div className="w-full
                 max-w-2xl bg-gray-800 text-gray-100">
                     <h1 className="text-2xl font-bold text-center">Usuario no encontrado</h1>
